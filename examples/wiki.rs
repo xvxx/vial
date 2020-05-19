a0691fbf1e365a5aec5eacad4abbb52084e39ab4
chris west <c@xvxx.io>
2020-05-19 15:31:55 -0700

    wiki example

---
+++ examples/wiki.rs
@@ -0,0 +1,54 @@
+use vial;
+
+mod wiki {
+    use vial::{vial, Request, Response};
+
+    vial! {
+        GET "/" => index;
+        GET "/about" => about;
+
+        GET "/new" => new;
+        POST "/new" => create;
+
+        GET "/:page/edit" => edit;
+        POST "/:page" => update;
+        GET "/:page" => show;
+        GET "/:page.md" => show_raw;
+    }
+
+    fn index(req: Request) -> Response {
+        "".into()
+    }
+
+    fn about(req: Request) -> Response {
+        "".into()
+    }
+
+    fn new(req: Request) -> Response {
+        "".into()
+    }
+
+    fn create(req: Request) -> Response {
+        "".into()
+    }
+
+    fn edit(req: Request) -> Response {
+        "".into()
+    }
+
+    fn update(req: Request) -> Response {
+        "".into()
+    }
+
+    fn show(req: Request) -> Response {
+        "".into()
+    }
+
+    fn show_raw(req: Request) -> Response {
+        "".into()
+    }
+}
+
+fn main() {
+    vial::run!("0.0.0.0:4567", wiki);
+}
