dbab439a5930db9f642aec21df258bc017beda4e
chris west <c@xvxx.io>
2020-05-25 23:37:10 -0700

    getting closer

--- src/asset.rs
+++ src/asset.rs
@@ -46,22 +46,30 @@ pub fn normalize_path(path: &str) -> Option<String> {
 /// (or in the embedded fs, in release mode).
 pub fn exists(path: &str) -> bool {
     if let Some(path) = normalize_path(path) {
-        if let Ok(mut file) = fs::File::open(path) {
-            if let Ok(meta) = file.metadata() {
-                return !meta.is_dir();
+        println!("EXISTS? {}", path);
+        #[cfg(not(bundle_assets))]
+        {
+            if let Ok(mut file) = fs::File::open(path) {
+                if let Ok(meta) = file.metadata() {
+                    return !meta.is_dir();
+                }
             }
         }
+
+        println!("DOES IT? {:?}", unsafe {
+            crate::BUNDLED_ASSETS.as_ref().unwrap().contains_key(&path)
+        });
+        #[cfg(bundle_assets)]
+        return unsafe { crate::BUNDLED_ASSETS.contains_key(path) };
     }
     false
 }
 
 /// Like fs::read_to_string(), but with an asset.
 pub fn to_string(path: &str) -> Result<String> {
-    if let Some(path) = normalize_path(path) {
-        if let Some(bytes) = read(&path) {
-            if let Ok(utf8) = str::from_utf8(bytes.as_ref()) {
-                return Ok(utf8.to_string());
-            }
+    if let Some(bytes) = read(path) {
+        if let Ok(utf8) = str::from_utf8(bytes.as_ref()) {
+            return Ok(utf8.to_string());
         }
     }
 
@@ -74,12 +82,21 @@ pub fn to_string(path: &str) -> Result<String> {
 /// Read a file to [u8].
 pub fn read(path: &str) -> Option<Cow<'static, [u8]>> {
     let path = normalize_path(path)?;
-    let mut buf = vec![];
-    if let Ok(mut file) = fs::File::open(path) {
-        if let Ok(meta) = file.metadata() {
-            if !meta.is_dir() && file.read_to_end(&mut buf).is_ok() {
-                return Some(Cow::from(buf));
+    #[cfg(not(bundle_assets))]
+    {
+        let mut buf = vec![];
+        if let Ok(mut file) = fs::File::open(path) {
+            if let Ok(meta) = file.metadata() {
+                if !meta.is_dir() && file.read_to_end(&mut buf).is_ok() {
+                    return Some(Cow::from(buf));
+                }
             }
+        }
+    }
+    #[cfg(bundle_assets)]
+    {
+        if let Some(v) = unsafe { vial::BUNDLED_ASSETS.get(path) } {
+            return Cow::from(v);
         }
     }
     None
--- src/bundler.rs
+++ src/bundler.rs
@@ -14,20 +14,30 @@ pub fn bundle_assets(dir: &str) -> Result<()> {
         let out_dir = env::var("OUT_DIR").unwrap();
         let mut dest = File::create(Path::new(&out_dir).join("bundle.rs"))?;
 
-        dest.write_all(b"#[macro_export]\n macro_rules! vial_bundled_assets {\n  () => {{\n")?;
-        dest.write_all(b"    let mut map = std::collections::HashMap::new();\n")?;
+        dest.write_all(
+            b"
+#[macro_export]
+macro_rules! vial_bundled_assets {
+    () => {{
+        let mut map = std::collections::HashMap::new();
+",
+        )?;
         for path in asset::iter(dir) {
-            println!("-> {:?}", path);
             dest.write_all(
                 format!(
-                    "    map.insert({:?}.into(), &include_bytes!(\"../{}\")[..]);\n",
+                    "        map.insert({:?}.into(), &include_bytes!(\"../{}\")[..]);\n",
                     path,
                     path.as_path().to_string_lossy()
                 )
                 .as_bytes(),
             );
         }
-        dest.write_all(b"    map\n  }};\n}")?;
+        dest.write_all(
+            b"
+            map
+    }};
+}",
+        )?;
         println!("cargo:rustc-cfg=bundle_assets");
         println!("cargo:rustc-cfg=release");
     }
