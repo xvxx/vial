49b8f8282ff3e03645e0c3f01e382c64dc5c9e86
chris west <c@xvxx.io>
2020-06-07 15:16:53 -0700

    fix mod example

--- examples/mods/wiki.rs
+++ examples/mods/wiki.rs
@@ -1,51 +1,49 @@
 #![allow(unused)]
 use vial;
 
-mod wiki {
-    use vial::prelude::*;
+use vial::prelude::*;
 
-    routes! {
-        GET "/" => index;
-        GET "/about" => about;
+routes! {
+    GET "/" => index;
+    GET "/about" => about;
 
-        GET "/new" => new;
-        POST "/new" => create;
+    GET "/new" => new;
+    POST "/new" => create;
 
-        GET "/:page/edit" => edit;
-        POST "/:page" => update;
-        GET "/:page" => show;
-        GET "/:page.md" => show_raw;
-    }
+    GET "/:page/edit" => edit;
+    POST "/:page" => update;
+    GET "/:page" => show;
+    GET "/:page.md" => show_raw;
+}
 
-    fn index(req: Request) -> impl Responder {
-        ""
-    }
+fn index(req: Request) -> impl Responder {
+    ""
+}
 
-    fn about(req: Request) -> impl Responder {
-        ""
-    }
+fn about(req: Request) -> impl Responder {
+    ""
+}
 
-    fn new(req: Request) -> impl Responder {
-        ""
-    }
+fn new(req: Request) -> impl Responder {
+    ""
+}
 
-    fn create(req: Request) -> impl Responder {
-        ""
-    }
+fn create(req: Request) -> impl Responder {
+    ""
+}
 
-    fn edit(req: Request) -> impl Responder {
-        ""
-    }
+fn edit(req: Request) -> impl Responder {
+    ""
+}
 
-    fn update(req: Request) -> impl Responder {
-        ""
-    }
+fn update(req: Request) -> impl Responder {
+    ""
+}
 
-    fn show(req: Request) -> impl Responder {
-        ""
-    }
+fn show(req: Request) -> impl Responder {
+    ""
+}
 
-    fn show_raw(req: Request) -> impl Responder {
-        ""
-    }
+fn show_raw(req: Request) -> impl Responder {
+    ""
 }
