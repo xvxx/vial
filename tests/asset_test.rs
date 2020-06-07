0da21f6bd90b0d03178a9f58584acc0c95c6ad7a
chris west <c@xvxx.io>
2020-06-06 19:13:51 -0700

    work on different machines

--- tests/asset_test.rs
+++ tests/asset_test.rs
@@ -13,11 +13,8 @@ fn asset_exists() {
 #[test]
 fn etag_test() {
     vial::asset_dir!("./tests/assets/");
-
-    assert_eq!("49aef7a87da06fb1", asset::etag("dinner.jpg"));
-    assert_eq!("2a711aad1e6358e4", asset::etag("letter.jpg"));
-    assert_eq!("74fdd19ee00e2b24", asset::etag("puff.gif"));
-    assert_eq!("bd60acb658c79e45", asset::etag("made-up.gif"));
+    assert_ne!(asset::etag("dinner.jpg"), asset::etag("letter.jpg"));
+    assert_ne!(asset::etag("dinner.jpg"), asset::etag("made-up.gif"));
 }
 
 #[test]
