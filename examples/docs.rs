5e1ebc51c27a9215be365b2942b9a9ef9f54c087
chris west <c@xvxx.io>
2020-06-07 20:12:48 -0700

    docs example

---
+++ examples/docs.rs
@@ -0,0 +1,21 @@
+use {std::thread, vial::prelude::*};
+
+routes! {
+    GET "/" => |_| {
+        Response::from_file("docs/index.html")
+    };
+}
+
+fn main() {
+    asset_dir!("docs/");
+
+    thread::spawn(|| loop {
+        let mut child = std::process::Command::new("watch")
+            .args(&["make", "docs"])
+            .spawn()
+            .unwrap();
+        child.wait().unwrap();
+    });
+
+    run!().unwrap();
+}
