d30206352a38c480785a4a3f4ac5c3104d7e7393
chris west <c@xvxx.io>
2020-06-09 11:10:49 -0700

    store IO error source, do our own ==

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,7 @@
 use std::{error::Error as ErrorTrait, fmt, io};
 
 /// Possible Vial errors.
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug)]
 pub enum Error {
     /// Can't find an asset.
     AssetNotFound(String),
@@ -20,7 +20,7 @@ pub enum Error {
     /// Failed to parse HTTP request.
     ParseError,
     /// io::Error
-    IO(String),
+    IO(io::Error),
     /// Unknown error.
     Other(String),
 }
@@ -33,29 +33,81 @@ impl From<Error> for io::Error {
 
 impl From<io::Error> for Error {
     fn from(err: io::Error) -> Error {
-        Error::IO(err.to_string())
+        Error::IO(err)
     }
 }
 
 impl ErrorTrait for Error {
-    fn description(&self) -> &str {
+    fn source(&self) -> Option<&(dyn ErrorTrait + 'static)> {
         match self {
-            Error::UnknownHTTPMethod(reason) => &reason,
-            Error::ConnectionClosed => "Connetion Closed By Client",
-            Error::ParseVersion => "Error Parsing HTTP Version",
-            Error::ExpectedCRLF => "Expected CRLF in HTTP Request",
-            Error::ParseHeaderName => "Error Parsing HTTP Header name",
-            Error::ParseHeaderValue => "Error Parsing HTTP Header value",
-            Error::ParseError => "Error Parsing HTTP Request",
-            Error::AssetNotFound(..) => "Can't Find Asset",
-            Error::IO(..) => "io::Error While Parsing HTTP Request",
-            Error::Other(reason) => &reason,
+            Error::IO(source) => Some(source),
+            _ => None,
         }
     }
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "vial::Error: {}", self)
+        write!(
+            f,
+            "{}",
+            match self {
+                Error::UnknownHTTPMethod(reason) => &reason,
+                Error::ConnectionClosed => "Connection Closed By Client",
+                Error::ParseVersion => "Error Parsing HTTP Version",
+                Error::ExpectedCRLF => "Expected CRLF in HTTP Request",
+                Error::ParseHeaderName => "Error Parsing HTTP Header name",
+                Error::ParseHeaderValue => "Error Parsing HTTP Header value",
+                Error::ParseError => "Error Parsing HTTP Request",
+                Error::AssetNotFound(..) => "Can't Find Asset",
+                Error::IO(..) => "io::Error While Parsing HTTP Request",
+                Error::Other(reason) => &reason,
+            }
+        )
+    }
+}
+
+impl PartialEq for Error {
+    fn eq(&self, other: &Self) -> bool {
+        use Error::*;
+        match self {
+            IO(_) => return false,
+            AssetNotFound(s) => match other {
+                AssetNotFound(o) => s == o,
+                _ => false,
+            },
+            UnknownHTTPMethod(s) => match other {
+                UnknownHTTPMethod(o) => s == o,
+                _ => false,
+            },
+            Other(s) => match other {
+                Other(o) => s == o,
+                _ => false,
+            },
+            ConnectionClosed => match other {
+                ConnectionClosed => true,
+                _ => false,
+            },
+            ParseVersion => match other {
+                ParseVersion => true,
+                _ => false,
+            },
+            ExpectedCRLF => match other {
+                ExpectedCRLF => true,
+                _ => false,
+            },
+            ParseHeaderName => match other {
+                ParseHeaderName => true,
+                _ => false,
+            },
+            ParseHeaderValue => match other {
+                ParseHeaderValue => true,
+                _ => false,
+            },
+            ParseError => match other {
+                ParseError => true,
+                _ => false,
+            },
+        }
     }
 }
