4f81f74fb1f0061de7a7f1396d2af591163f1116
chris west <c@xvxx.io>
2020-10-10 10:15:26 -0700

    add usage hint to JSON example

--- examples/json.rs
+++ examples/json.rs
@@ -1,7 +1,14 @@
 use vial::prelude::*;
 
 routes! {
+    GET "/" => form;
     POST "/json" => post;
+}
+
+fn form(_: Request) -> impl Responder {
+    r#"Do this: <pre>
+curl -d '{"message": "Hello, Vial!"}' -H "Content-Type: application/json" -X POST http://0.0.0.0:7667/json
+"#
 }
 
 fn post(req: Request) -> impl Responder {
