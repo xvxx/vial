eececbc520a9ae84335a469295639af88ceb5cb3
Jason E Bailey <jason.bailey@24601.org>
2021-10-30 10:41:30 -0400

    Request handling changes
    Consume raw request at a single go and process without breaking for
     additional data. Results in various changes to support the concept.

--- tests/httparse_test.rs
+++ tests/httparse_test.rs
@@ -2,7 +2,7 @@
 // https://github.com/seanmonstar/httparse/blob/master/tests/uri.rs
 
 use vial::{
-    http_parser::{parse, Status},
+    http_parser::parse,
     Error,
 };
 
@@ -11,15 +11,15 @@ macro_rules! test {
         #[test]
         fn $name() {
             use vial::{
-                http_parser::{parse, Status},
+                http_parser::parse,
                 Request,
             };
 
-            let req = match parse($buf.to_vec()).unwrap() {
-                Status::Complete(request) => request,
-                Status::Partial(buf) => panic!(
-                    "Expected Status::Complete, got Status::Partial({})",
-                    buf.len()
+            let req = match parse($buf.to_vec()) {
+                Ok(request) => request,
+                Err(e) => panic!(
+                    "Expected Status::Complete, got Err - {})",
+                    e
                 ),
             };
             closure(req);
@@ -104,7 +104,7 @@ test! {
 test! {
     urltest_007,
     b"GET  foo.com HTTP/1.1\r\nHost: \r\n\r\n",
-    Error::ParseVersion
+    Error::ParsePath
 }
 
 test! {
@@ -2814,7 +2814,7 @@ test! {
 #[test]
 fn test_request_partial() {
     match parse(b"GET / HTTP/1.1\r\n\r".to_vec()) {
-        Ok(Status::Partial(..)) => assert!(true),
+        Err(Error::ConnectionClosed) => assert!(true),
         _ => assert!(false),
     }
 }
@@ -2822,7 +2822,7 @@ fn test_request_partial() {
 #[test]
 fn test_request_partial_version() {
     match parse(b"GET / HTTP/1.".to_vec()) {
-        Ok(Status::Partial(..)) => assert!(true),
+        Err(Error::ConnectionClosed)=> assert!(true),
         _ => assert!(false),
     }
 }
