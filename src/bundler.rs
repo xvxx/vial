e6ceab70272a175020def6651d32d50cce403263
sigaloid <69441971+sigaloid@users.noreply.github.com>
2021-11-06 15:12:45 -0400

    Fix error

--- src/bundler.rs
+++ src/bundler.rs
@@ -32,7 +32,7 @@ pub fn bundle_assets(dir: &str) -> Result<()> {
         if link.exists() {
             fs::remove_file(&link)?;
         }
-        unix::fs::symlink(env::current_dir()?.join(dir), link)?;
+        std::os::unix::fs::symlink(env::current_dir()?.join(dir), link)?;
         let bundle_rs = Path::new(&out_dir).join("bundle.rs");
         if bundle_rs.exists() {
             fs::remove_file(&bundle_rs)?;
