f4470fa078950ce4c0fbba84603613ee56099f27
Matthew E <matt@matthew.science>
2022-11-15 16:23:51 -0500

    Resize thread pool and set TcpStream timeout (#10)
    * Resize thread pool

--- src/server.rs
+++ src/server.rs
@@ -4,22 +4,23 @@ use {
         io::Write,
         net::{TcpListener, TcpStream, ToSocketAddrs},
         sync::{Arc, Mutex},
+        time::Duration,
     },
     threadpool::ThreadPool,
 };
 
-const MAX_CONNECTIONS: usize = 10;
+const INITIAL_THREADS: usize = 10;
+const MAXIMUM_THREADS: usize = 400;
 
 /// Starts a new Vial server. Should always be invoked via the
 /// [`vial::run!()`](macro.run.html) macro, since there is some setup
 /// that needs to happen.
 #[doc(hidden)]
 pub fn run<T: ToSocketAddrs>(addr: T, router: Router, banner: Option<&str>) -> Result<()> {
-    let pool = ThreadPool::new(MAX_CONNECTIONS);
+    let mut pool = ThreadPool::new(INITIAL_THREADS);
     let listener = TcpListener::bind(&addr)?;
     let addr = listener.local_addr()?;
     let server = Arc::new(Server::new(router));
-
     #[cfg(feature = "state")]
     eprintln!("! vial feature `state` is now built-in. You can safely remove it.");
 
@@ -32,6 +33,16 @@ pub fn run<T: ToSocketAddrs>(addr: T, router: Router, banner: Option<&str>) -> R
     }
 
     for stream in listener.incoming() {
+        // if all threads are active, extend by two
+        if pool.active_count() > pool.max_count() - 1 && pool.max_count() < MAXIMUM_THREADS {
+            pool.set_num_threads(pool.max_count() + 2);
+        }
+        // tldr: if no active threads and the threadpool has
+        // been expanded, halve the total number of threads.
+        if pool.active_count() == 0 && pool.max_count() > INITIAL_THREADS * 2 {
+            pool.set_num_threads(pool.max_count() / 2);
+        }
+
         let server = server.clone();
         let stream = stream?;
         pool.execute(move || {
@@ -55,6 +66,10 @@ impl Server {
 
     fn handle_request(&self, stream: TcpStream) -> Result<()> {
         let reader = stream.try_clone()?;
+
+        //discard because: https://doc.rust-lang.org/std/net/struct.TcpStream.html#method.set_read_timeout
+        let _ = reader.set_read_timeout(Some(Duration::from_millis(1000)));
+
         let req = Request::from_reader(reader)?;
         self.write_response(stream, req)
     }
