e73ba087fad88b9f1edfabd33c8ebb805f55ad7b
CuriouslyCurious <thecuriouslycurious@protonmail.com>
2023-09-04 02:12:20 +0200

    Wrap mock_test in a module

--- tests/mock_test.rs
+++ tests/mock_test.rs
@@ -1,48 +1,46 @@
 #[cfg(feature = "mock")]
-use {
-    minreq::{Method, Request},
-    std::{
-        net::{Ipv4Addr, SocketAddrV4},
-        thread,
-    },
-    vial::{Response, Router, Server},
-};
+mod mock {
+    use {
+        minreq::{Method, Request},
+        std::{
+            net::{Ipv4Addr, SocketAddrV4},
+            thread,
+        },
+        vial::{Response, Router, Server},
+    };
 
-#[cfg(feature = "mock")]
-fn make_request(router: Router, method: Method, url: &str) -> minreq::Response {
-    let addr = SocketAddrV4::new(Ipv4Addr::LOCALHOST, 0);
-    let mut server = Server::new(router, addr.into(), 1, None);
-    let request_addr = format!("http://{}{}", server.addr(), url);
-    let request = Request::new(method, request_addr);
-    thread::spawn(move || {
-        server.run_once();
-    });
-    let resp = request.send().unwrap();
+    fn make_request(router: Router, method: Method, url: &str) -> minreq::Response {
+        let addr = SocketAddrV4::new(Ipv4Addr::LOCALHOST, 0);
+        let mut server = Server::new(router, addr.into(), 1, None);
+        let request_addr = format!("http://{}{}", server.addr(), url);
+        let request = Request::new(method, request_addr);
+        thread::spawn(move || {
+            server.run_once();
+        });
+        let resp = request.send().unwrap();
 
-    resp
-}
+        resp
+    }
 
-#[cfg(feature = "mock")]
-fn string_body(_: vial::Request) -> Response {
-    "Hello, World!".into()
-}
+    fn string_body(_: vial::Request) -> Response {
+        "Hello, World!".into()
+    }
 
-#[test]
-#[cfg(feature = "mock")]
-fn mock_get() {
-    let mut router = Router::new();
-    router.insert("GET", "/get", string_body);
-    let resp = make_request(router, Method::Get, "/get");
-    assert_eq!(resp.status_code, 200);
-    assert_eq!(resp.as_str().unwrap(), "Hello, World!")
-}
+    #[test]
+    fn get() {
+        let mut router = Router::new();
+        router.insert("GET", "/get", string_body);
+        let resp = make_request(router, Method::Get, "/get");
+        assert_eq!(resp.status_code, 200);
+        assert_eq!(resp.as_str().unwrap(), "Hello, World!")
+    }
 
-#[test]
-#[cfg(feature = "mock")]
-fn mock_post() {
-    let mut router = Router::new();
-    router.insert("POST", "/post", string_body);
-    let resp = make_request(router, Method::Post, "/post");
-    assert_eq!(resp.status_code, 200);
-    assert_eq!(resp.as_str().unwrap(), "Hello, World!")
+    #[test]
+    fn post() {
+        let mut router = Router::new();
+        router.insert("POST", "/post", string_body);
+        let resp = make_request(router, Method::Post, "/post");
+        assert_eq!(resp.status_code, 200);
+        assert_eq!(resp.as_str().unwrap(), "Hello, World!")
+    }
 }
